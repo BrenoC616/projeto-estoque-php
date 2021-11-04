fb77b1447b2f5367d4a4535328326e8fad4ea9f3
Breno C <80727930+BrenoC616@users.noreply.github.com>
2021-11-03 22:22:07 -0300

    Criando Pagina de Cadastro

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <a class="nav-link" href="#"> Sobre </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"> Cadastro </a>
+            <a class="nav-link" href="./pages/cadastro.php"> Cadastro </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#"> Políticas </a>
@@ -45,7 +45,7 @@
         <div class="col-sm-4">
           <div class="card">
             <article class="card-body">
-              <a class="float-right btn btn-outline-primary" href="#"
+              <a class="float-right btn btn-outline-primary" href="./pages/cadastro.php"
                 >Registrar</a
               >
               <h4 class="card-title mb-4 mt-1">Entrar</h4>
